7b74853dbe233d14b8209f91b3b2162865359f27
kirstypurden <kirsty.purden.2015@uni.strath.ac.uk>
2017-03-24 12:50:26 +0000

    Files with leave after and arrive before functions

--- EE273-Undertale-Project/Journey Planner/Journey Planner/Travel.hpp
+++ EE273-Undertale-Project/Journey Planner/Journey Planner/Travel.hpp
@@ -1,16 +1,29 @@
 #pragma once
 #include "Destination.hpp"
+#include<iostream>
+#include<windows.h>
+
+
 class Travel
 {
 public:
 	Travel();
 	~Travel();
-	virtual void pathfinding_algorithm(Destination* start_dest, Destination* end_dest);
+	virtual float pathfinding_algorithm(Destination* start_dest, Destination* end_dest);
+	void leaveAfter(float);
+	void arriveBefore(float);
+	float calcJourneyTime(float);
+
+
 protected:
 	double total_route_time;
-	double medium_speed;
+	//double mean_speed=0;
 	int no_stops;
+	//float departTimes[1];
+	//std::string type = "NULL";
 	Destination* array_of_avoids;
-
+	std::string type = "Bus";
+	float mean_speed = 20;
+	double departTimes[20] = { 8.05, 9.12, 10.15, 10.40, 11.15, 12.01, 12.21, 12.41, 13.01, 13.19, 13.56, 14.14, 15.08, 15.47, 16.00, 16.17, 16.38, 17.00, 17.05, 18.30 };
 };
 
