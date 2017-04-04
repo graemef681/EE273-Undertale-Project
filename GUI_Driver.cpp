6c4ee3c20506822e118a0fefcd0e0d6173612e65
kirstypurden <kirsty.purden.2015@uni.strath.ac.uk>
2017-04-04 15:11:05 +0100

    Only small changes, but stops infinite loop

--- GUI_Driver.cpp
+++ GUI_Driver.cpp
@@ -4,78 +4,112 @@
 #include <fstream>
 #include <SFML/Graphics.hpp>
 #include "Top.hpp"
-#include"GUI_functions.hpp"
+#include "GUI_functions.hpp"
+#include "Dog.hpp"
+#include "Walker.hpp"
 using namespace std;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(250, 250), "GUI Driver");
+	sf::RenderWindow window(sf::VideoMode(640, 490), "GUI Driver");
+	window.setFramerateLimit(60);
+	sf::RectangleShape Background;
+	Background.setSize(sf::Vector2f(window.getSize()));
+	sf::Texture BackgroundTexture;
+	// Let's create the background image here, where everything initializes.
+	if (!BackgroundTexture.loadFromFile("C:/Users/minusfractions/Documents/Code_For_Easy_Access/background.png"))
+		cout << "Texture couldnt be read" << endl;
+	else
+		Background.setTexture(&BackgroundTexture);
+
+
+	sf::RectangleShape TopologyBox({ 427,490 });
+	sf::RectangleShape TopBackground;
+	TopBackground.setSize(sf::Vector2f(TopologyBox.getSize()));
+	sf::Texture TopTexture;
+	if (!TopTexture.loadFromFile("C:/Users/minusfractions/Documents/Code_For_Easy_Access/topbackground.png"))
+		cout << "Texture couldnt be read" << endl;
+	else
+		TopBackground.setTexture(&TopTexture);
+
+
+	TopologyBox.setPosition({ 0,0 });
+	TopologyBox.setFillColor(sf::Color::Transparent);
+	TopologyBox.setOutlineColor(sf::Color::Black);
+	TopologyBox.setOutlineThickness(3);
+	list<Destination> Buttons;
+	Destination AddButton({ 75,50 }, { 480,60 }, "AddButton", 0, 0);
+	AddButton.setFillColor(sf::Color::Green);
+	Destination DelButton({ 75,50 }, { 580,60 }, "DelButton", 0, 0);
+	DelButton.setFillColor(sf::Color::Red);
+	Destination WalkButton({ 75,75 }, { 530,210 }, "WalkButton", 0, 0);
+	WalkButton.setFillColor(sf::Color::Yellow);
+	Destination BusButton({ 75,75 }, { 530,285 }, "BusButton", 0, 0);
+	BusButton.setFillColor(sf::Color::Blue);
+	Destination TrainButton({ 75,75 }, { 530,360 }, "TrainButton", 0, 0);
+	TrainButton.setFillColor(sf::Color::Magenta);
+	Buttons.push_back(WalkButton);
+	Buttons.push_back(BusButton);
+	Buttons.push_back(TrainButton);
+	Buttons.push_back(AddButton);
+	Buttons.push_back(DelButton);
+
+	Dog dog;
+	int i = 0, mode = 0;
+
 	list<Destination> Top;
 	Top.clear();
 	Top = ReadFile("Node_topology");
 
-	//addNewDest("Node_topology",&Top);
-	//deleteDest(&Top,"Waterfall","Node_topology");
-
-
+	sf::Texture nodeTexture;
+	if (!nodeTexture.loadFromFile("C:/Users/minusfractions/Documents/Code_For_Easy_Access/node.png"))
+		cout << "Texture couldnt be read" << endl;
 	//Window event and draw loop
 
-	list<Destination>::iterator begin = Top.begin(), end = Top.end(), It, It2;
 
-	It = begin;
-	It2 = It++;
-	while (It != end)
-	{
-		cout << It->distance_from(*It2) << endl;
-		It++;
-	}
 	bool selected = false;
+	bool adding = false;
+	bool deleting = false;
+	Destination startDest, endDest;
 	while (window.isOpen())
 	{
+		list<Destination>::iterator It;
+		It = Top.begin();
+		while (It != Top.end())
+		{
+			It->setTexture(&nodeTexture);
+			It++;
+		}
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
-		}
-		window.clear();
-		using namespace std;
-
-		list<Destination>::iterator begin = Top.begin(), end = Top.end(), It;
-
-		It = begin;
-		while (It != end)
-		{
-			window.draw(*It);
-			It++;
-		}
-		window.display();
-
-		//get position of mouse when it is clicked
-		bool blInBox = false;
-		Destination temp, startDest, endDest;; //instance to return destination clicked on
-		if (event.type == sf::Event::MouseButtonReleased)
-		{
-			if (event.mouseButton.button == sf::Mouse::Left)
+			//get position of mouse when it is clicked
+			bool blInBox = false;
+			bool ButtInBox = false;
+			Destination temp; //instance to return destination clicked on
+			if (event.type == sf::Event::MouseButtonReleased)
 			{
-				
-
-				cout << "CLICK" << endl;
-				sf::Vector2i position = sf::Mouse::getPosition(window);
-				//call checkInBox function
-				//remember to pass in psotion 
-				blInBox = inBox(&Top,position,&temp);
-				cout << "blinbox is:" << blInBox << endl;
-			}
-		}//end of click event
-			if (blInBox)
-			{
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					cout << "CLICK" << endl;
+					sf::Vector2i position = sf::Mouse::getPosition(window);
+					//call checkInBox function
+					//remember to pass in position 
+					blInBox = inBox(&Top, position, &temp);
+					ButtInBox = inBox(&Buttons, position, &temp);
+					cout << "blinbox is:" << blInBox << endl;
+					cout << "ButtInBox is: " << ButtInBox << endl;
+				}
+				if (blInBox)
+				{
 					//if the destination is the first selected
 					if (selected == false)
 					{
 						//set it as the start
-						Destination startDest = temp;
-						cout << "START DESTINATION SET:"<<startDest.getName() << endl;
+						startDest = temp;
+						cout << "START DESTINATION SET:" << startDest.getName() << endl;
 						cout << "Selected is" << selected << endl;
 						selected = true;
 					}
@@ -84,19 +118,95 @@ int main()
 						if (startDest.getName() != temp.getName())
 						{
 							//otherwise, set it as the end
-							Destination endDest = temp;
-							cout << "END DESTINATION SET:"<<endDest.getName() << endl;
+							 endDest = temp;
+							cout << "END DESTINATION SET:" << endDest.getName() << endl;
 							cout << "Selected is" << selected << endl;
+							Walker test;
+							test.pathfinding_algorithm(startDest, endDest, &Top);
 						}
+						//;
 					}
 					//this is where start and end would be put in the pathfinder
-			}
-			selected = false;
+				}
+				if (ButtInBox)
+				{
+					if (temp.getName() == "AddButton" && deleting != true)
+					{
+						cout << "\n\n\nAdd button pressed" << endl;
+						adding = true;
+						OpenAddWindow();
+						addNewDest("Node_Topology", &Top);
+						Top.clear();
+						Top = ReadFile("Node_Topology");
+						cout << "File read." << endl;
+					}
+					else if (temp.getName() == "DelButton" && adding != true)
+					{
+						deleting = true;
+					}
+					else if (temp.getName() == "WalkButton")
+					{
+						mode = 0; cout << "Mode is: " << mode << endl;
+					}
+					else if (temp.getName() == "BusButton")
+					{
+						mode = 1; cout << "Mode is: " << mode << endl;
+					}
+					else if (temp.getName() == "TrainButton")
+					{
+						mode = 2; cout << "Mode is: " << mode << endl;
+					}
+				}
+				if (deleting)
+				{
+					cout << "Temp Name: " << temp.getName() << endl;
+					cout << "Click the destination you want to delete!\n";
+					list<Destination>::iterator TBegin = Top.begin(), Tend = Top.end();
+					It = Tend;
+					while (It != TBegin)
+					{
+						It--;
+						cout << "It->getName(): " << It->getName() << endl;
+						if (It->getName() == temp.getName())
+						{
+							deleteDest(&Top, temp.getName(), "Node_topology");
+							deleting = false;
+							break;
+						}
+
+					}
+				}
+			}//end of click event
+		}
+
+		window.clear();
+		window.draw(Background);
+		window.draw(TopBackground);
+
+		It = Top.begin();
+		while (It != Top.end())
+		{
+
+			window.draw(*It);
+			It++;
+		}
+		list<Destination>::iterator Bit;
+		Bit = Buttons.begin();
+		while (Bit != Buttons.end())
+		{
+			window.draw(*Bit);
+			Bit++;
+		}
+		window.draw(TopologyBox);
+		//Dog sprite animation
+		if (i < 12000)
+			i++;
+		else if (i == 12000)
+			i = 0;
+		window.draw(dog.Draw(i));
+		window.display();
 	}//end of window loop
-	
-	
+
+	Top.clear();
 	return 0;
 }
-
-
-
---
+++ Travel.cpp
@@ -0,0 +1,282 @@
+#include "Travel.hpp"
+
+
+Travel::Travel()
+{
+	total_route_time = 0.0;
+	mean_speed= 0.0;
+	no_stops = 0;
+}
+
+
+Travel::~Travel()
+{
+}
+
+
+float Travel::pathfinding_algorithm(Destination start_dest, Destination end_dest, std::list<Destination>* Top)
+{
+		using namespace std;
+		//create required sets
+		list<Destination> journey, *neighbours;
+		list<float> distance;
+		//empty list before use
+		journey.clear();
+		//set start destination to be the first in the journey list
+		journey.push_back(start_dest);
+		//list<Destination>::iterator DestBegin = top.begin(), DestEnd = top.end(), DestIt;
+		//DestIt = DestBegin;
+		double prevDistance = 10000000000000, curDistance, shortestDistance = 10000000000000, total = 0;
+		Destination shortest, temp;
+		shortest.setName("Temp Shortest");
+		Destination curNode;
+		curNode = start_dest;
+		std::cout << "Start dest is:" << start_dest.getName() << endl;
+		std::cout << "End dest is:" << end_dest.getName() << endl;
+		int count = 1;
+		while (shortest.getName() != end_dest.getName())
+		{
+			prevDistance = 10000000000000;
+			shortestDistance = 10000000000000;
+			bool visitedBefore = false;
+			std::cout << "START OF WHILE" << endl;
+			std::cout << "Current node is: " << curNode.getName();
+			neighbours = curNode.getNeighbourList();
+			std::cout << neighbours->size() << endl;
+			list<Destination>::iterator begin = neighbours->begin(), end = neighbours->end(), NIt;
+			NIt = begin;
+			while (NIt != end)
+			{
+				//If not visited before then check distance from. 
+				curDistance = curNode.distance_from(*NIt);
+				std::cout << "Distance from " << curNode.getName() << " to " << NIt->getName() << ":" << curDistance << endl;
+				if (curDistance < prevDistance)
+				{ //if the currentDistance is shorter than the previous
+					shortest = *NIt;
+					std::cout << "Number of neighbours for shortest is: " << shortest.getNoNeighbours();
+					shortestDistance = curDistance;
+				}
+				prevDistance = curDistance;
+				NIt++;
+			}
+			list<Destination>::iterator Jbegin = journey.begin(), Jend = journey.end(), Jit;
+			Jit = Jbegin;
+			while (Jit != Jend)
+			{
+				if ((*Jit).getName() == shortest.getName())
+				{
+					visitedBefore = true;
+				}
+				temp = *Jit;
+				Jit++;
+			}
+			if (!visitedBefore)
+			{
+				journey.push_back(shortest);
+				distance.push_back(shortestDistance);
+				curNode = shortest;
+				std::cout << "The current node is now:" << shortest.getName() << endl;
+				//visitedBefore = false;
+			}
+		}
+		journey.push_back(end_dest);
+		distance.push_back((end_dest).distance_from(temp));
+		list<Destination>::iterator jBegin = journey.begin(), jEnd = journey.end(), jIt;
+		jIt = jBegin;
+		std::cout << "THE SHORTEST JOURNEY IS" << endl;
+		while (jIt != jEnd)
+		{
+			std::cout << jIt->getName() << endl;
+			jIt++;
+		}
+
+		list<float>::iterator DBegin = distance.begin(), DEnd = distance.end(), DIt;
+		DIt = DBegin;
+		while (DIt != DEnd)
+		{
+			total = total + *DIt;
+			DIt++;
+		}
+		std::cout << "THE DISTANCE IS :" << total << endl;
+
+		calcJourneyTime(total);
+		return total;
+}
+
+void Travel::leaveAfter(float journeyTime)
+{
+	float hour, min;
+	//get the time the user wants to leave after
+	std::cout << "Please give the hour you want to leave after:" << std::endl;
+	std::cin >> hour;
+	std::cout << "Please give the minutes you want to leave after:" << std::endl;
+	std::cin >> min;
+	float leaveTime = hour + (min / 100);
+
+
+	//get current time
+	SYSTEMTIME time;
+	GetLocalTime(&time);
+	float curHour, curMin;
+	curHour = time.wHour;
+	curMin = time.wMinute;
+	float curTime = curHour + (curMin / 100);
+	std::cout.precision(2);
+	std::cout.setf(std::ios::fixed);
+	std::cout << "The current time is " << curHour << ":" << curMin << std::endl;
+	// compare the hours
+	if (hour < curHour)
+	{//if the leave after time is before the current time
+		std::cout << "Invalid time : cannot choose time in the past." << std::endl;
+	}
+	else if (hour == curHour)
+	{//if the leave after time is within the same hour, check the minutes
+		if (min < curMin)
+		{//if the minutes is before the current time
+			std::cout << "Invalid time: cannot choose time in the past." << std::endl;
+		}
+		else if (min >= curMin)
+		{// if the minutes is in the future, choose the next avaiable time
+			std::cout << "The leave time is this hour" << std::endl;
+			for (int i = 0; i < 20; i++)
+			{
+				std::cout.precision(2);
+				std::cout.setf(std::ios::fixed);
+				std::cout << "The leave time is " << leaveTime;
+				std::cout << " -- The depart time is " << departTimes[i] << std::endl;
+				if (departTimes[i] > leaveTime)
+				{//if the departTime is in the future 
+						//set it as next available journey
+						std::cout << "The next available " << type << " leaves at " << departTimes[i] << std::endl;
+						break;
+				}
+			}
+		}
+	}
+	else if (hour > curHour)
+	{//if the leave after time is in a future hour
+		//the next available time is the first one that hour
+		std::cout << "The leave time is a future hour" << std::endl;
+	
+			for (int i = 0; i < 20; i++)
+			{
+				std::cout.precision(2);
+				std::cout.setf(std::ios::fixed);
+				std::cout << "The leave time is " << leaveTime;
+				std::cout << " -- The depart time is " << departTimes[i] << std::endl;
+				if (departTimes[i] > leaveTime)
+				{//if the departTime is in the future 
+				 // if the time until then + journey time is < the time it arrives - current time
+						//set it as next available journey
+						std::cout << "The next available " << type << " leaves at " << departTimes[i] << std::endl;
+						break;
+					}
+				}
+			}
+	}
+
+void Travel::arriveBefore(float journeyTime)
+{
+	float hour, min;
+	//get the time the user wants to arrive before
+	std::cout << "Please give the hour you want to arrive before:" << std::endl;
+	std::cin >> hour;
+	std::cout << "Please give the minutes you want to arrive before:" << std::endl;
+	std::cin >> min;
+	float beforeTime = hour + (min / 100);
+
+	//get current time
+	SYSTEMTIME time;
+	GetLocalTime(&time);
+	float curHour, curMin;
+	curHour = time.wHour;
+	curMin = time.wMinute;
+	float curTime = curHour + (curMin / 100);
+	std::cout.precision(2);
+	std::cout.setf(std::ios::fixed);
+	std::cout << "The current time is " << curTime << std::endl;
+	// compare the hours
+	if (hour < curHour)
+	{//if the arrive before time is before the current time
+		std::cout << "Invalid time : cannot choose time in the past (hours)." << std::endl;
+	}
+	else if (hour == curHour)
+	{//if the arrive before time is within the same hour, check the minutes
+		if (min < curMin)
+		{//if the minutes is before the current time
+			std::cout << "Invalid time: cannot choose time in the past (mins)." << std::endl;
+		}
+		else if (min > curMin)
+		{// if the minutes is in the future, choose the next avaiable time
+		 //greater than the current time
+			std::cout << "The arrive before time is this hour" << std::endl;
+
+			for (int i = 0; i < 20; i++)
+			{
+				std::cout.precision(2);
+				std::cout.setf(std::ios::fixed);
+				std::cout << "The arrive before time is " << beforeTime;
+				std::cout << " -- The current time is " << curTime;
+				std::cout << " -- The depart time is " << departTimes[i] << std::endl;
+				if (departTimes[i] < beforeTime && departTimes[i]>curTime)
+				{//if the departTime is in the future 
+				 // if the time until then + journey time is < the time it arrives - current time
+					if ((beforeTime - journeyTime) < curTime)
+					{
+						//this journey is ok
+						std::cout << "The next available " << type << " getting to your destination is " << departTimes[i] << std::endl;
+						break;
+					}
+					else
+					{
+						std::cout << "There are no transports leaving before your desired times." << std::endl;
+					}
+				}
+			}
+
+		}
+		else if (hour > curHour)
+		{//if the arrive before time is in a future hour
+		 //the next available time is the first one that hour
+			std::cout << "The arrive before time is a future hour" << std::endl;
+
+			for (int i = 0; i < 20; i++)
+			{
+				std::cout.precision(2);
+				std::cout.setf(std::ios::fixed);
+				std::cout << "The arrive before time is " << beforeTime;
+				std::cout << " -- The current time is " << curTime;
+				std::cout << " -- The depart time is " << departTimes[i] << std::endl;
+				if (departTimes[i] < beforeTime && departTimes[i]>curTime)
+				{//if the departTime is in the future 
+				 // if the time until then + journey time is < the time it arrives - current time
+					if ((beforeTime - journeyTime) < curTime)
+					{
+						//this journey is ok
+						std::cout << "The next available " << type << " getting to your destination is " << departTimes[i] << std::endl;
+						break;
+					}
+					else
+					{
+						std::cout << "There are no transports leaving before your desired times." << std::endl;
+					}
+				}
+			}
+		}
+	}
+}
+
+
+float Travel::calcJourneyTime(float distance)
+{
+	float time, hour=0, min=0, journeyTime;
+	time = distance / mean_speed;
+
+	if (time > 1)
+	{//if time greater than 1 hour
+		hour = int(time);
+		min = time - 1 * 60;
+	}
+	journeyTime = hour + (min / 100);
+	return journeyTime;
+}
